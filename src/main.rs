f701adb8f740977c2d219ace6da772c2583fe384
OtadTOAD <90140046+OtadTOAD@users.noreply.github.com>
2025-06-10 06:51:26 +0400

    Added specific capabilities without using default

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,33 @@ impl LanguageServer for Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
         Ok(InitializeResult {
             capabilities: ServerCapabilities {
+                semantic_tokens_provider: None,
+                hover_provider: None,
+                signature_help_provider: None,
+                selection_range_provider: None,
+                definition_provider: None,
+                type_definition_provider: None,
+                implementation_provider: None,
+                references_provider: None,
+                document_highlight_provider: None,
+                document_symbol_provider: None,
+                workspace_symbol_provider: None,
+                code_action_provider: None,
+                code_lens_provider: None,
+                document_formatting_provider: None,
+                document_range_formatting_provider: None,
+                document_on_type_formatting_provider: None,
+                rename_provider: None,
+                document_link_provider: None,
+                color_provider: None,
+                folding_range_provider: None,
+                declaration_provider: None,
+                workspace: None,
+                call_hierarchy_provider: None,
+                moniker_provider: None,
+                linked_editing_range_provider: None,
+                experimental: None,
+
                 text_document_sync: Some(TextDocumentSyncCapability::Kind(
                     TextDocumentSyncKind::FULL,
                 )),
@@ -48,11 +75,10 @@ impl LanguageServer for Backend {
                         "{".to_string(),
                         "`".to_string(),
                         "'".to_string(),
-                        "[[".to_string(),
+                        "[".to_string(),
                     ]),
                     ..Default::default()
                 }),
-                ..Default::default()
             },
             server_info: None,
         })
